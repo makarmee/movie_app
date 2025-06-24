b69475a409122ea177889af48eed93033f0fa4eb
makarmee <mmahdikarimi2006@gmail.com>
2025-06-24 18:49:19 +0430

    default colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./App.tsx", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#030014',
+        secondary:'#151312',
+        light:{
+          100:'#d6c6ff',
+          200:'#a8b5db',
+          300:'#9ca4ab',
+        },
+        dark:{
+          100:'#221f3d',
+          200:'#0f0d23',
+        },
+        accent:'#ab8bff'
+      }
+    },
   },
   plugins: [],
 }
